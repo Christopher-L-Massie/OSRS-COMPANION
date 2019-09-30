c1855fbfa662e19d977906490b7c7aa592593e0c
DonFranquito <50225179+DonFranquito@users.noreply.github.com>
2019-09-30 01:51:17 -0500

    Began building display table for player current statistics/progress(coming soon)

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/HomeController.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/HomeController.java
@@ -1,17 +1,29 @@
 package com.example.OSRSCOMPANION.controllers;
 
 
+import com.example.OSRSCOMPANION.models.data.PlayerDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/")
 public class HomeController {
 
+    @Autowired
+    private PlayerDao playerDao;
+
     @RequestMapping(value = "")
     public String index(){
         return "home/index";
     }
 
+    @RequestMapping(value = "player")
+    public String playerStats(Model model){
+
+        //temp test for building table
+        model.addAttribute("player",playerDao.findById(1).get());
+        return "home/player";}
 
 }
