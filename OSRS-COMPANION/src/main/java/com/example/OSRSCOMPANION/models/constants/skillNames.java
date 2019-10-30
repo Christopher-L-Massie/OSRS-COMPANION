102aa52718f04472d72ddcf947760f6f75204f63
DonFranquito <50225179+DonFranquito@users.noreply.github.com>
2019-10-30 04:27:17 -0500

    Added skillNumber property to enum skillNames to easily fetch data from datapoints without needing to remember the skill number

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/skillNames.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/skillNames.java
@@ -2,36 +2,45 @@ package com.example.OSRSCOMPANION.models.constants;
 
 public enum skillNames {
 
-    OVERALL("overall"),
-    ATTACK("attack"),
-    DEFENCE("defence"),
-    STRENGTH("strength"),
-    HITPOINTS("hitpoints"),
-    RANGED("ranged"),
-    PRAYER("prayer"),
-    MAGIC("magic"),
-    COOKING("cooking"),
-    WOODCUTTING("woodcutting"),
-    FLETCHING("fletching"),
-    FISHING("fishing"),
-    FIREMAKING("firemaking"),
-    CRAFTING("crafting"),
-    SMITHING("smithing"),
-    MINING("mining"),
-    HERBLORE("herblore"),
-    AGILITY("agility"),
-    THIEVING("thieving"),
-    SLAYER("slayer"),
-    FARMING("farming"),
-    RUNECRAFTING("runecrafting"),
-    HUNTER("hunter"),
-    CONSTRUCTION("construction");
+    OVERALL("overall",0),
+    ATTACK("attack",1),
+    DEFENCE("defence",2),
+    STRENGTH("strength",3),
+    HITPOINTS("hitpoints",4),
+    RANGED("ranged",5),
+    PRAYER("prayer",6),
+    MAGIC("magic",7),
+    COOKING("cooking",8),
+    WOODCUTTING("woodcutting",9),
+    FLETCHING("fletching",10),
+    FISHING("fishing",11),
+    FIREMAKING("firemaking",12),
+    CRAFTING("crafting",13),
+    SMITHING("smithing",14),
+    MINING("mining",15),
+    HERBLORE("herblore",16),
+    AGILITY("agility",17),
+    THIEVING("thieving",18),
+    SLAYER("slayer",19),
+    FARMING("farming",20),
+    RUNECRAFTING("runecrafting",21),
+    HUNTER("hunter",22),
+    CONSTRUCTION("construction",23);
+
+
+    //properties
 
     private String skillName;
+    private int skillNumber;
 
-    private skillNames(String skillName){
+    //constructors
+
+    private skillNames(String skillName, int skillNumber){
         this.skillName = skillName;
+        this.skillNumber = skillNumber;
     }
+
+    //accessors
 
     public String getSkillName(){
         return skillName;
@@ -41,4 +50,11 @@ public enum skillNames {
         this.skillName = skillName;
     }
 
+    public int getSkillNumber() {
+        return skillNumber;
+    }
+
+    public void setSkillNumber(int skillNumber) {
+        this.skillNumber = skillNumber;
+    }
 }
