5d72a54f0880cfa34f8edd74ae49f7f7d5efc403
DonFranquito <50225179+DonFranquito@users.noreply.github.com>
2019-12-02 03:05:37 -0600

    Fixed getters for Normal,Ironman,Ultimate,Hardcore Data. They were returning a list without specifying that they're holding <DataPoint> types.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/Player.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/Player.java
@@ -384,13 +384,13 @@ public class Player {
 
     public Timestamp getLastUpdated() {return lastUpdated;}
 
-    public List getNormalData(){return this.normalData;}
+    public List<DataPoint> getNormalData(){return this.normalData;}
 
-    public List getIronmanData(){return this.ironmanData;}
+    public List<DataPoint> getIronmanData(){return this.ironmanData;}
 
-    public List getUltimateData(){return this.ultimateData;}
+    public List<DataPoint> getUltimateData(){return this.ultimateData;}
 
-    public List getHardcoreData(){return this.hardcoreData;}
+    public List<DataPoint> getHardcoreData(){return this.hardcoreData;}
 
     public boolean getIsNormal(){return this.isNormal;}
 
