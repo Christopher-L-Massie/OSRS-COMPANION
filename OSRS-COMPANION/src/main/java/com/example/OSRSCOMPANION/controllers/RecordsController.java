10bb2cc7ca702c11defb49e7bf3e1d9703ef3a97
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-01-28 17:24:22 -0600

    Began work on record calculating but decided to wait until I restructure the backend to be more efficient(hopefully!) :)

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/RecordsController.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/RecordsController.java
@@ -1,10 +1,14 @@
 package com.example.OSRSCOMPANION.controllers;
 
+import com.example.OSRSCOMPANION.models.Player;
 import com.example.OSRSCOMPANION.models.data.PlayerDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "records")
@@ -17,6 +21,11 @@ public class RecordsController {
     public String index(Model model){
         model.addAttribute("title","Tournaments");
 
+        List<Player> players = new ArrayList<>();
+
+        for (Player player : playerDao.findAll()){
+            players.add(player);
+        }
         return "records/records";
     }
 }
