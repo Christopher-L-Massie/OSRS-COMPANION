fdf637483157afed2ce54a6802a6da1c412a8e46
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-03 18:57:05 -0600

    Worked on the clueNames enum. It was pretty empty because I think I abandoned this early on to try to get enough finished for launch code but now that i'm free of those restrictions I can do the full support that I wanted to from the beginning.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/clueNames.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/clueNames.java
@@ -2,12 +2,42 @@ package com.example.OSRSCOMPANION.models.constants;
 
 public enum clueNames {
 
-    ALL,
-    BEGINNER,
-    EASY,
-    MEDIUM,
-    HARD,
-    ELITE,
-    MASTER
+    //|||VALUES|||
 
+    BEGINNER("beginner",0),
+    EASY("easy",1),
+    MEDIUM("medium",2),
+    HARD("hard",3),
+    ELITE("elite",4),
+    MASTER("master",5);
+
+    //|||PROPERTIES|||
+
+    private String clueName;
+    private Integer clueType;
+
+    //|||CONSTRUCTORS|||
+
+    private clueNames(String clueName, Integer clueType){
+        this.clueName = clueName;
+        this.clueType = clueType;
+    }
+
+    //|||ACCESSORS|||
+
+    public String getClueName() {
+        return clueName;
+    }
+
+    public void setClueName(String clueName) {
+        this.clueName = clueName;
+    }
+
+    public Integer getClueType() {
+        return clueType;
+    }
+
+    public void setClueType(Integer clueType) {
+        this.clueType = clueType;
+    }
 }
