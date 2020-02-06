f5fd5fb495fe12aa1ce1899804493d6035f23562
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-06 17:23:45 -0600

    added properties to the bossData class

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/databuilder/bossData.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/databuilder/bossData.java
@@ -1,4 +1,20 @@
 package com.example.OSRSCOMPANION.models.databuilder;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class bossData {
+
+    //|||PROPERTIES|||
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String bossName;
+    private Integer bossNumber;
+    private long bossRank;
+    private long bossScore;
 }
