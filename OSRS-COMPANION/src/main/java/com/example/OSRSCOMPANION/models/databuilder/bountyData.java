8caa0a59b51401b021a95efb4566aff082b517da
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-05 19:28:38 -0600

    added properties to bountyData class, for storing the various types of bounty data.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/databuilder/bountyData.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/databuilder/bountyData.java
@@ -1,4 +1,19 @@
 package com.example.OSRSCOMPANION.models.databuilder;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class bountyData {
+
+    //|||PROPERTIES|||
+    
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public String bountyType;
+    public long bountyRank;
+    public long bountyScore;
 }
