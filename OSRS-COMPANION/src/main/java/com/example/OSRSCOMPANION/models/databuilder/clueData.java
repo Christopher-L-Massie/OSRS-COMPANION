a070414ac01733b77a4a3b2e8409ba8b03fc3976
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-05 19:34:22 -0600

    added basic properties that the clueData class will need to get it's job done.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/databuilder/clueData.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/databuilder/clueData.java
@@ -1,4 +1,19 @@
 package com.example.OSRSCOMPANION.models.databuilder;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class clueData {
+
+    //|||PROPERTIES|||
+
+    @Id
+    @GeneratedValue
+    public int id;
+
+    public Integer clueType;
+    public long clueRank;
+    public long clueScore;
 }
