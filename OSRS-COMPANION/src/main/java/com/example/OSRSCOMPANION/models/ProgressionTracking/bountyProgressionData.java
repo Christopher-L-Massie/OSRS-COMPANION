756c585bca199b411b6d76d5de92224b9af7f5ed
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-06 19:31:28 -0600

    added properties to the bountyProgressionData class

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/ProgressionTracking/bountyProgressionData.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/ProgressionTracking/bountyProgressionData.java
@@ -1,4 +1,18 @@
 package com.example.OSRSCOMPANION.models.ProgressionTracking;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class bountyProgressionData {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private Integer bountyTypeNumber;
+    private String bountyTypeName;
+    private long bountyRankChange;
+    private long bountyScoreChange;
 }
