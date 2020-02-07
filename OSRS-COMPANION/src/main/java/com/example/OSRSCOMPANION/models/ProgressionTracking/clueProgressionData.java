abe84fc498a139207837f5c4fe6161d845999d86
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-06 19:27:05 -0600

    added constructors to the clueProgressionData class

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/ProgressionTracking/clueProgressionData.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/ProgressionTracking/clueProgressionData.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 @Entity
 public class clueProgressionData {
 
+    //|||Properties|||
+
     @Id
     @GeneratedValue
     private int id;
@@ -16,4 +18,17 @@ public class clueProgressionData {
     private long rankChange;
     private long scoreChange;
     private long days;
+
+    //|||CONSTRUCTORS|||
+    public clueProgressionData(){}
+
+    public clueProgressionData(Integer clueTypeNumber,String clueTypeName,long rankChange,long scoreChange,long days){
+        this.clueTypeName = clueTypeName;
+        this.clueTypeName = clueTypeName;
+        this.rankChange = rankChange;
+        this.scoreChange = scoreChange;
+        this.days = days;
+    }
+    
+
 }
