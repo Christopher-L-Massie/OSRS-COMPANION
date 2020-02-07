9c09ecf74483083037e5e1035d4d0e999c350526
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-07 16:58:55 -0600

    added properties to the lmsProgressionData

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/ProgressionTracking/lmsProgressionData.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/ProgressionTracking/lmsProgressionData.java
@@ -1,8 +1,19 @@
 package com.example.OSRSCOMPANION.models.ProgressionTracking;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class lmsProgressionData {
+
+    //|||PROPERTIES|||
+
+    @Id
+    @GeneratedValue
+    private int id;
     
+    private long lmsRankChange;
+    private long lmsScoreChange;
+
 }
