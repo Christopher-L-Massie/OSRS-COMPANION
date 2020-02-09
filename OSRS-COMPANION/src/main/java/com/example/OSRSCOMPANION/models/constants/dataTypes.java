010b4b1c84e0b884bc93aaaf901349ba7960362a
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-08 20:37:04 -0600

    :sparkles:Added Values to dataTypes enum. The order is important because it will make it possible to condense my loops a lot. The UnknownData is also important to work around a 'bug'? or just forgotten dataPoint that jagex is sending out and I can't figure out what it is tracking.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/dataTypes.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/dataTypes.java
@@ -1,4 +1,13 @@
 package com.example.OSRSCOMPANION.models.constants;
 
 public enum dataTypes {
+
+    //|||VALUES|||
+
+    SKILLDATA,
+    UNKNOWNDATA,
+    BOUNTYDATA,
+    CLUEDATA,
+    LMSDATA,
+    BOSSDATA
 }
