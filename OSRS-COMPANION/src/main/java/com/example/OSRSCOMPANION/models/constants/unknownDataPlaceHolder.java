a688d2f23a928e23857e4ea0f960233e6530ea26
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-09 16:29:36 -0600

    :hammer:Filled in the unknownDataPlaceHolder enum with a name so that it should work with in the loops now :)

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/unknownDataPlaceHolder.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/unknownDataPlaceHolder.java
@@ -1,4 +1,28 @@
 package com.example.OSRSCOMPANION.models.constants;
 
 public enum unknownDataPlaceHolder {
+    
+    //|||VALUES|||
+
+    UKNOWN("Unknown");
+
+    //|||PROPERTIES|||
+
+    private String name;
+
+    //|||CONSTRUCTORS|||
+
+    unknownDataPlaceHolder(String name){
+        this.name = name;
+    }
+
+    //|||ACCESSORS|||
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
