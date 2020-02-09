f7cd667ba7ccf5c3cde73482cf5f50146164f56a
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-08 20:56:52 -0600

    :sparkles:Added properties to data class

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/databuilder/data.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/databuilder/data.java
@@ -1,4 +1,19 @@
 package com.example.OSRSCOMPANION.models.databuilder;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class data {
+
+    //|||PROPERTIES|||
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+    private int dataType;
+    private long rank;
+    private long score;
 }
