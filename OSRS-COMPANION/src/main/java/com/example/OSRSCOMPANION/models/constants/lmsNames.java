85a486da7e0988bee64a5dd6e7f9016a6e2c0402
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-09 16:35:59 -0600

    :hammer:Filled in the lmsNames enum.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/lmsNames.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/lmsNames.java
@@ -1,4 +1,28 @@
 package com.example.OSRSCOMPANION.models.constants;
 
 public enum lmsNames {
+
+    //|||VALUES|||
+
+    LMS("Last Man Standing");
+
+    //|||PROPERTIES|||
+
+    private String name;
+
+    //|||CONSTRUCTORS|||
+
+    lmsNames(String name){
+        this.name = name;
+    }
+
+    //|||ACCESSORS|||
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
