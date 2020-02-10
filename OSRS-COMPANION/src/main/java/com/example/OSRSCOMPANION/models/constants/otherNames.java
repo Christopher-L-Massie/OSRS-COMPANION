a15814ff5c9931a227d2a344d4e5a9f0efb18bea
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-10 16:17:33 -0600

    :sparkles:Added accessors to otherNames enum

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/otherNames.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/otherNames.java
@@ -69,4 +69,21 @@ public class otherNames {
         this.typeNumber = typeNumber;
     }
 
+    //|||ACCESSORS|||
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTypeNumber() {
+        return typeNumber;
+    }
+
+    public void setTypeNumber(int typeNumber) {
+        this.typeNumber = typeNumber;
+    }
 }
