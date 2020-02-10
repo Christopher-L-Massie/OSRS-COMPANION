abaf05f1b51d50cb185b13d0ef82e6406b32cfe5
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-10 15:40:06 -0600

    :hammer:Added properties to the ProgressionData class

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/ProgressionTracking/ProgressionData.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/ProgressionTracking/ProgressionData.java
@@ -1,4 +1,20 @@
 package com.example.OSRSCOMPANION.models.ProgressionTracking;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class ProgressionData {
+
+    //|||PROPERTIES|||
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+    private int dataType;
+    private long rankChange;
+    private long scoreChange;
 }
