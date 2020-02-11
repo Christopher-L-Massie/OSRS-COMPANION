47c6b61183e1676ca53a7a27b200292a06656c04
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-11 16:07:52 -0600

    :hammer:added starting properties to entry class will fill this out as I think of more useful information

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/entry.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/entry.java
@@ -1,7 +1,20 @@
 package com.example.OSRSCOMPANION.models.merchantHelp;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class entry {
+
+    //|||Properties|||
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String itemName;
+    private long amountBought;
+    private long buyPrice;
+    private long sellPrice;
+    private long margin;
 }
