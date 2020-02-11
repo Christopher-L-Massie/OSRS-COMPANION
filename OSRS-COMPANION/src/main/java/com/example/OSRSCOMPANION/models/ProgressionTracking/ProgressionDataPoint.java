a913babe15a7acf80b393c774f4f1c1dd4148eb4
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-11 15:34:54 -0600

    :sparkles:Added new methods to ProgressionDataPoint

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/ProgressionTracking/ProgressionDataPoint.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/ProgressionTracking/ProgressionDataPoint.java
@@ -54,6 +54,17 @@ public class ProgressionDataPoint {
         this.days = days;
     }
 
+    public void addDataProgressionData(ProgressionData progressionData, long days){
+        this.progressionData.add(progressionData);
+        this.days = days;
+    }
+
+    public void addProgressionData(skillProgressionData skillProgressionData, ProgressionData progressionData, long days){
+        this.skillProgressionData.add(skillProgressionData);
+        this.progressionData.add(progressionData);
+        this.days = days;
+    }
+
     //|||ACCESSORS|||
 
     public int getId() {
