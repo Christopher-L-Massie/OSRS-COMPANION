661beb6987f40a9750d594d5d3841dbc439b0b81
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-11 16:02:24 -0600

    :construction:Filled in the log class for the merchantHelper package. Will need more to it but this is a good starting point to build off of.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/log.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/log.java
@@ -1,7 +1,45 @@
 package com.example.OSRSCOMPANION.models.merchantHelp;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class log {
+
+    //|||PROPERTIES|||
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String owner;
+    private long gold;
+
+    //|||CONSTRUCTORS|||
+    public log(){}
+
+    public log(String owner,long startingGold){
+        this.owner = owner;
+        this.gold = startingGold;
+    }
+
+    //|||METHODS|||
+
+
+    //|||ACCESSORS|||
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public long getGold() {
+        return gold;
+    }
+
+    public void setGold(long gold) {
+        this.gold = gold;
+    }
 }
