9f9b26e08be0cb7ff67d85f7e88e4b3a89213037
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-12 18:19:55 -0600

    :sparkles:Created MerchantController that will handle all request that have to do with the merchant log system and any further addons but I'd like to keep this simple as just a small little test build of a smaller system to take a break from working on the data and progression system which has gotten a bit complicated recently.

---
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/MerchantController.java
@@ -0,0 +1,9 @@
+package com.example.OSRSCOMPANION.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "")
+public class MerchantController {
+}
