6ed488399da239e01a12a80d678876753f213314
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-12 18:53:20 -0600

    :hammer:Changed the path that the MerchantController is looking at. root/merchant/x

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/MerchantController.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/MerchantController.java
@@ -1,9 +1,17 @@
 package com.example.OSRSCOMPANION.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "")
+@RequestMapping(value = "merchant")
 public class MerchantController {
+
+    @RequestMapping(value = "logs")
+    public String index(Model model){
+        model.addAttribute("title","Merchant Logs");
+
+        return "merchants/merchant_logs";
+    }
 }
