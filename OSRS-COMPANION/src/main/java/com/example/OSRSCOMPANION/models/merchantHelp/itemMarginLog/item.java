bdf744f42fd305d2d087e8d8fe74802a9cbeb1c7
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-14 12:48:42 -0600

    :zap:Created accessors in the item class.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/itemMarginLog/item.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/itemMarginLog/item.java
@@ -27,5 +27,28 @@ public class item {
         this.buyLimit = buyLimit;
     }
 
+    //|||Accessors|||
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getBuyLimit() {
+        return buyLimit;
+    }
+
+    public void setBuyLimit(long buyLimit) {
+        this.buyLimit = buyLimit;
+    }
+
+    public List<check> getPriceChecks() {
+        return priceChecks;
+    }
+
+    public void setPriceChecks(List<check> priceChecks) {
+        this.priceChecks = priceChecks;
+    }
 }
