85c0b08b9d65708e2c3e7f61de39c44796fcb2bd
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-13 19:40:39 -0600

    :sparkles:Fleshed out the itemLog class a bit further.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/itemMarginLog/itemLog.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/itemMarginLog/itemLog.java
@@ -1,7 +1,37 @@
 package com.example.OSRSCOMPANION.models.merchantHelp.itemMarginLog;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class itemLog {
+
+    //|||PROPERTIES|||
+    private int id;
+
+    private String owner;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<item> trackedItems = new ArrayList<>();
+
+    //|||CONSTRUCTORS|||
+    public itemLog(){}
+
+    public itemLog(String owner){
+        this.owner = owner;
+    }
+
+    //|||METHODS|||
+
+    //|||ACCESSORS|||
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
