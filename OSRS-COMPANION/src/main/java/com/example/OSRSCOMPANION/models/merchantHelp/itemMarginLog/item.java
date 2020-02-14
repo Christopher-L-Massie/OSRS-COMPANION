dc91e297ae2ceb87865dbd06f49395a8dab327f1
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-14 12:45:07 -0600

    :sparkles:Added properties to item class

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/itemMarginLog/item.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/itemMarginLog/item.java
@@ -1,7 +1,25 @@
 package com.example.OSRSCOMPANION.models.merchantHelp.itemMarginLog;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class item {
+
+    //|||Properties|||
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    private long buyLimit;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<check> priceChecks = new ArrayList<>();
+
+    
+
+
 }
