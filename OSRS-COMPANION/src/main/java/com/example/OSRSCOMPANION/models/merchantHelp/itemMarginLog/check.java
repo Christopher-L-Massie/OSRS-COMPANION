71addb9a80432ba4532e735939d0c9497c839fda
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-14 12:50:59 -0600

    :sparkles:Added Properties to check class

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/itemMarginLog/check.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/itemMarginLog/check.java
@@ -1,7 +1,24 @@
 package com.example.OSRSCOMPANION.models.merchantHelp.itemMarginLog;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.sql.Timestamp;
 
 @Entity
 public class check {
+
+    //|||Properties|||
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private long buyPrice;
+
+    private long sellPrice;
+
+    private long margin;
+
+    private Timestamp timeChecked;
 }
