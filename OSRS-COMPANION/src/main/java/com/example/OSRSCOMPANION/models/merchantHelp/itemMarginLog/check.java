3d6bf0b66fbd15d4d3a9d57d1613da5b7972a0cc
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-14 13:01:39 -0600

    :zap:Added accessors to the check class

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/itemMarginLog/check.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/itemMarginLog/check.java
@@ -31,4 +31,39 @@ public class check {
         this.margin = sellPrice - buyPrice;
         this.timeChecked = new Timestamp(System.currentTimeMillis());
     }
+
+    //|||Methods|||
+
+    //|||Accessors|||
+    public long getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(long buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public long getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(long sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public long getMargin() {
+        return margin;
+    }
+
+    public void setMargin(long margin) {
+        this.margin = margin;
+    }
+
+    public Timestamp getTimeChecked() {
+        return timeChecked;
+    }
+
+    public void setTimeChecked(Timestamp timeChecked) {
+        this.timeChecked = timeChecked;
+    }
 }
