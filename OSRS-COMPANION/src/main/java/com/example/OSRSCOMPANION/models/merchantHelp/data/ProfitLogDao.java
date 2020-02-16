22f42ca234715973b85cc83d39f3a88c063ad9a4
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-15 18:51:18 -0600

    :hammer:Renamed 'LogDao' to 'ProfitLogDao' for consistency.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/data/ProfitLogDao.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/data/ProfitLogDao.java
@@ -1,9 +1,9 @@
 package com.example.OSRSCOMPANION.models.merchantHelp.data;
 
-import com.example.OSRSCOMPANION.models.merchantHelp.profitLog.log;
+import com.example.OSRSCOMPANION.models.merchantHelp.profitLog.profitLog;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LogDao extends CrudRepository<log, Integer> {
+public interface ProfitLogDao extends CrudRepository<profitLog, Integer> {
 }
