cd48b2888c0bcba116e5a448a855ab02988b6a2e
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-20 11:35:28 -0600

    :truck:Moved the CreateMerchantMarginLogForm to the already existing forms folder and deleted the other form folder that I had made by mistake when looking in the wrong folder thinking I had deleted the forms folder.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/forms/CreateMerchantMarginLogForm.java
+++
@@ -1,9 +0,0 @@
-package com.example.OSRSCOMPANION.forms;
-
-public class CreateMerchantMarginLogForm {
-
-
-    //|||PROPERTIES|||
-
-    
-}
---
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/forms/CreateMerchantMarginLogForm.java
@@ -0,0 +1,15 @@
+package com.example.OSRSCOMPANION.models.forms;
+
+import com.example.OSRSCOMPANION.models.merchantHelp.itemMarginLog.marginLog;
+
+public class CreateMerchantMarginLogForm {
+
+
+    //|||PROPERTIES|||
+
+    private marginLog itemLog;
+
+    private String logName;
+
+
+}
