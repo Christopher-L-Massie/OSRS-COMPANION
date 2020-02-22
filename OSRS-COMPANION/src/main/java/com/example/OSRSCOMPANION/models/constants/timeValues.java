0172f0ebf3ad9d169a9cf5af8d2dd3c857ea5528
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-21 22:25:11 -0600

    :construction:Added two new values to the timeValues (These may not work, we will see but in theory they should work as is. If they do I will refactor the other code because I like this design for it more).

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/timeValues.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/constants/timeValues.java
@@ -6,7 +6,9 @@ public enum timeValues {
     MINUTE(60000),
     HOUR(3600000),
     DAY(86400000),
-    WEEK(604800000);
+    WEEK(604800000),
+    MONTH(WEEK.getMilliseconds()* 4),
+    YEAR(MONTH.getMilliseconds() * 12);
 
     //|||PROPERTIES|||
 
