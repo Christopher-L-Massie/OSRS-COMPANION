95065e511509306b11062534cfc4585c9bcef0ef
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-23 22:55:11 -0600

    :hammer:Created a method for the UserController to handle login request. The form on the html side needs to be fleshed out.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/UserController.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.example.OSRSCOMPANION.controllers;
 import com.example.OSRSCOMPANION.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +12,11 @@ public class UserController {
 
     @Autowired
     private UserDao userDao;
+
+    @RequestMapping(value = "login")
+    public String userLoginDisplay(Model model){
+        model.addAttribute("title","Login");
+        
+        return "accounts/loginUserAccount";
+    }
 }
