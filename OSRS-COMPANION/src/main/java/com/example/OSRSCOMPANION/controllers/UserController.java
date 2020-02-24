b573dfa403d0433d8114baa4f5f52fe0d8fe3816
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-23 22:52:04 -0600

    :hammer:Setup the UserController and autowired the UserDao as it will be needed as more pages and features are developed that make use of users.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/UserController.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/UserController.java
@@ -1,4 +1,14 @@
 package com.example.OSRSCOMPANION.controllers;
 
+import com.example.OSRSCOMPANION.models.data.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "user")
 public class UserController {
+
+    @Autowired
+    private UserDao userDao;
 }
