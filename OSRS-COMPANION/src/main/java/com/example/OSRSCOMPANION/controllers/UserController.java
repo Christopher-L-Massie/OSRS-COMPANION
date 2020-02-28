da3b3204055dfbcfc1d1bbdbc5e055887b50525f
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-28 15:33:06 -0600

    :zap:Corrected the userLoginDisplay handler in UserController to insert an empty User object when user go to login. I think I may need to handle this differently but we'll get there when we get there.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/UserController.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/controllers/UserController.java
@@ -1,10 +1,12 @@
 package com.example.OSRSCOMPANION.controllers;
 
+import com.example.OSRSCOMPANION.models.User;
 import com.example.OSRSCOMPANION.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "user")
@@ -13,10 +15,11 @@ public class UserController {
     @Autowired
     private UserDao userDao;
 
-    @RequestMapping(value = "login")
+    @RequestMapping(value = "login", method = RequestMethod.GET)
     public String userLoginDisplay(Model model){
         model.addAttribute("title","Login");
-        
+        model.addAttribute("User", new User());
+
         return "accounts/loginUserAccount";
     }
 }
