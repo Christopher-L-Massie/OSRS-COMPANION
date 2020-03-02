01b5b51ad264514dea291901831423c599020c5e
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-03-01 18:44:04 -0600

    :sparkles:Created an EditCodeDao so that I can interact with the codes already made.

---
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/data/EditCodeDao.java
@@ -0,0 +1,7 @@
+package com.example.OSRSCOMPANION.models.merchantHelp.data;
+
+import com.example.OSRSCOMPANION.models.merchantHelp.editCode.editCode;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EditCodeDao extends CrudRepository<editCode, Integer> {
+}
