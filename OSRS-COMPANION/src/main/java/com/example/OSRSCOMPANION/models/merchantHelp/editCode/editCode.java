8d38e8a60588c8ba1ad95e9481859212a05e32d3
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-03-01 18:47:59 -0600

    :hammer:Added properties to the editCode class.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/editCode/editCode.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/editCode/editCode.java
@@ -1,7 +1,15 @@
 package com.example.OSRSCOMPANION.models.merchantHelp.editCode;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class editCode {
+
+    //|||Properties
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private code code;
 }
