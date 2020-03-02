5504e7bacb42d981be72d5598d5791b865dd0770
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-03-01 18:54:24 -0600

    :hammer:Generated accessors for the editCode class.

--- OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/editCode/editCode.java
+++ OSRS-COMPANION/src/main/java/com/example/OSRSCOMPANION/models/merchantHelp/editCode/editCode.java
@@ -11,11 +11,40 @@ public class editCode {
     @Autowired
     private CodeDao codeDao;
 
-    //|||Properties
+    //|||Properties|||
     @Id
     @GeneratedValue
     private int id;
 
     @OneToOne(cascade = CascadeType.ALL)
     private code code;
+
+    //0 = profit
+    //1 = margin
+    private int logType;
+
+    //|||Constructors|||
+    public editCode(){}
+
+    public editCode(int logType){
+    }
+
+    //|||Methods|||
+
+    //|||Accessors|||
+    public com.example.OSRSCOMPANION.models.merchantHelp.editCode.code getCode() {
+        return code;
+    }
+
+    public void setCode(com.example.OSRSCOMPANION.models.merchantHelp.editCode.code code) {
+        this.code = code;
+    }
+
+    public int getLogType() {
+        return logType;
+    }
+
+    public void setLogType(int logType) {
+        this.logType = logType;
+    }
 }
